f592fb1ee2cf1ee8dd2761b1039cab0551203b3e
Swapnil <swapnilpatil.8136@gmail.com>
2024-05-19 14:25:04 -0700

    design changes

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,11 +1,26 @@
 import Login from '@/components/Login'
 import Link from 'next/link'
+import HeaderLogo from '@/public/img/menu/nav/header-logo.png'
+import Image from 'next/image'
 
 const LoginPage = () => {
   return (
     <div className='h-screen w-screen flex justify-center items-center bg-slate-100'>
-      <div className='sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12'>
-        <h1 className='font-semibold text-4xl text-center'>LOGIN</h1>
+      <div className='sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-4'>
+        <div className='flex justify-center'>
+          {HeaderLogo ? (
+            <Image
+              src={HeaderLogo}
+              alt='Header Logo'
+              priority={true}
+              className='w-24 h-20'
+              width='0'
+              height='0'
+              sizes='100vw'
+            />
+          ) : null}
+        </div>
+        <h1 className='font-semibold text-4xl text-vb_blue-800 text-center tracking-widest'>LOGIN</h1>
         <Login />
         <p className='text-center'>
           Need to create an account?{' '}
--- pages/signup/index.jsx
+++ pages/signup/index.jsx
@@ -1,11 +1,27 @@
 import Signup from '@/components/Signup'
 import Link from 'next/link'
+import HeaderLogo from '@/public/img/menu/nav/header-logo.png'
+import Image from 'next/image'
 
 const SignupPage = () => {
   return (
     <div className='h-screen w-screen flex justify-center items-center bg-slate-100'>
-      <div className='sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12'>
-        <h1 className='font-semibold text-4xl text-center'>SIGNUP</h1>
+      <div className='sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-4'>
+        <div className='flex justify-center'>
+          {HeaderLogo ? (
+            <Image
+              src={HeaderLogo}
+              alt='Header Logo'
+              priority={true}
+              className='w-24 h-20'
+              width='0'
+              height='0'
+              sizes='100vw'
+            />
+          ) : null}
+        </div>
+        <h1 className='font-semibold text-4xl text-vb_blue-800 text-center tracking-widest'>SIGNUP</h1>
+        <div className=' border border-b-1 border-black mb-2'> </div>
         <Signup />
         <p className='text-center'>
           Already have an account?{' '}
